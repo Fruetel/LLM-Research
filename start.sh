756292b8a7a54660a1ecf5fac762744b0cca523c
Thomas Fruetel <github@fruetel.de>
2023-12-20 23:09:58 +0100

    Expose Llama port publicly

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ make LLAMA_CUBLAS=1
 
 wget https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/mixtral-8x7b-instruct-v0.1.Q5_0.gguf
 
-./server -m mixtral-8x7b-instruct-v0.1.Q5_0.gguf -ngl 33 -c 16384 --port 31336
+./server -m mixtral-8x7b-instruct-v0.1.Q5_0.gguf -ngl 33 -c 16384 --port 31336 --host 0.0.0.0
