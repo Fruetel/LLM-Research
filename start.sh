2a825d23804b9199fc1931f0811b93d1d3fa5ba9
Thomas Fruetel <github@fruetel.de>
2023-12-21 12:27:04 +0100

    Only download missing stuff

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/ggerganov/llama.cpp.git
+
+# Check if the llama.cpp directory does not exist
+if [ ! -d "llama.cpp" ]; then
+    git clone --depth 1 https://github.com/ggerganov/llama.cpp.git
+fi
+
 cd llama.cpp
 make LLAMA_CUBLAS=1
 
-wget https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/mixtral-8x7b-instruct-v0.1.Q5_0.gguf
+# Check if the .gguf file does not exist
+if [ ! -f "mixtral-8x7b-instruct-v0.1.Q5_0.gguf" ]; then
+    wget https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/mixtral-8x7b-instruct-v0.1.Q5_0.gguf
+fi
 
 ./server -m mixtral-8x7b-instruct-v0.1.Q5_0.gguf -ngl 33 -c 16384 --port 31336 --host 0.0.0.0
